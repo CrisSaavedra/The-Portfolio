c85bb7bfd932ce7d3db9b28b954761ff02bf0310
CrisSaavedra <crisisaave@gmail.com>
2023-03-23 20:04:17 -0300

    portafolio-first-commit

---
+++ src/Portafolio.jsx
@@ -0,0 +1,10 @@
+import { NavBar } from "./components/navBar/NavBar"
+
+export const Portafolio = () => {
+    return (
+        <div className="container">
+            {/* loader aca */}
+            <NavBar />
+        </div>
+    )
+}
---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,19 @@
+
+
+export const NavBar = () => {
+  return (
+    <div className="nav-container">
+        <div className="logo-container">
+            <h3>Crisdev</h3>
+        </div>
+
+        <div className="options-container">
+            <a href="#">home</a>
+            <a href="#">about</a>
+            <a href="#">projects</a>
+            <a href="#">contact</a>
+        </div>
+
+    </div>
+  )
+}
