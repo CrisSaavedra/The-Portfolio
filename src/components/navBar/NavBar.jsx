5d4b1ed1a1f426297b82d20203c175abbd8fe8d9
CrisSaavedra <crisisaave@gmail.com>
2023-03-24 01:16:30 -0300

    change color of background

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -4,10 +4,11 @@ export const NavBar = () => {
   return (
     <div className="nav-container">
         <div className="logo-container">
-            <h3>Crisdev</h3>
+            <h3>crisdev</h3>
         </div>
 
         <div className="options-container">
+           
             <a href="#">home</a>
             <a href="#">about</a>
             <a href="#">projects</a>
