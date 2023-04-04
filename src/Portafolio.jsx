3b8cba35cfddfa0b7bfef0a02f4dc7ac86e66115
CrisSaavedra <crisisaave@gmail.com>
2023-04-04 18:22:44 -0400

    projects done

--- src/Portafolio.jsx
+++ src/Portafolio.jsx
@@ -11,7 +11,7 @@ export const Portafolio = () => {
             <NavBar />
             <Home />
             <About/>
-            {/* <Projects/> */}
+            <Projects/>
         </div>
 
     )
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,14 +1,52 @@
+import weather from '../../assets/screens/weather.png';
+import moviet from '../../assets/screens/moviet.png';
+import gitHub from '../../assets/buttons/githubbtn.svg';
+import live from '../../assets/buttons/live3btn.svg';
 
 
 export const Projects = () => {
 
   return (
     <div className="projects-container">
-        <h3>Projects</h3>
+      <div className='title-project'>
+        <div className='line'></div>
+        <h3><span>P</span>rojects</h3>
+      </div>
 
-        <div className="display-projects">
-
+      {/* project 1  */}
+      <div className="display-projects">
+        <img className='screen' src={weather} alt="weather project img" />
+        <div className='text-container'>
+          <div className='text-project'>
+            <h5>THE WEATHER</h5>
+            <p>Search for a place and find its climate!</p>
+            <p className='techs-use'>REACT - TAILWIND</p>
+          </div>
+          <div className='btn-project'>
+            <a href="https://github.com/CrisSaavedra/The-simple-weather" target="_blank"><img src={gitHub} alt="github logo" width={20} />GITHUB</a>
+            <a href="https://the-simple-weather-csaave.netlify.app" target="_blank"><img src={live} alt="live logo" width={28} />LIVE</a>
+          </div>
         </div>
+      </div>
+
+      {/* project 2  */}
+
+      <div className="display-projects">
+        <img className='screen' src={moviet} alt="weather project img" />
+        <div className='text-container'>
+          <div className='text-project'>
+            <h5>Moviet</h5>
+            <p>search and save your favorite movies :)</p>
+            <p className='techs-use'>REACT - NODE - BOOTSTRAP</p>
+          </div>
+          <div className='btn-project'>
+            <a href="https://github.com/CrisSaavedra/Moviet" target="_blank"><img src={gitHub} alt="github logo" width={20} />GITHUB</a>
+            <a href="https://moviet.onrender.com" target="_blank"><img src={live} alt="live logo" width={28} />LIVE</a>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   )
 }
