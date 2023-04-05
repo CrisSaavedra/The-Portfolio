1a44bffcbde23f288a8f428665585709626e843b
CrisSaavedra <crisisaave@gmail.com>
2023-04-04 23:30:47 -0400

    El portafolio v1

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,21 +7,20 @@ export const Contact = () => {
     return (
         <div className='contact-container'>
             <div className='text-contact'>
-                <h3><span>C</span>ontact</h3>
                 <p>Don't by shy, contact me! :)</p>
             </div>
 
             <div className='contacts'>
                 <div className='contact'>
-                    <a href="#"> <img src={linked} alt="linked icon" width={20}/> LinkedIn</a>
+                    <a href="https://www.linkedin.com/in/c-saavedra-c" target="_blank"> <img src={linked} alt="linked icon" width={20}/> LinkedIn</a>
                 </div>
 
                 <div className='contact'>
-                    <a href="#"> <img src={git} alt="git icon" width={20}/> GitHub</a>
+                    <a href="https://github.com/CrisSaavedra" target="_blank"> <img src={git} alt="git icon" width={20}/> GitHub</a>
                 </div>
 
                 <div className='contact'>
-                    <a href="#"> <img src={email} alt=" mail icon" width={20}/> Email</a>
+                    <a href="mailto: crisisaave@gmail.com" target="_blank"> <img src={email} alt=" mail icon" width={20}/> Email</a>
                 </div>
 
 
