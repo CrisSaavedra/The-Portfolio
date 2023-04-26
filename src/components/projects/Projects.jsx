d40b151a72ee61fc40d5d41e8764780b4029d1f6
CrisSaavedra <crisisaave@gmail.com>
2023-04-26 19:15:23 -0400

    project update

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,5 +1,6 @@
 import weather from '../../assets/screens/weather.png';
 import moviet from '../../assets/screens/moviet.png';
+import humano from '../../assets/screens/humano.png';
 import gitHub from '../../assets/buttons/githubbtn.svg';
 import live from '../../assets/buttons/live3btn.svg';
 
@@ -14,6 +15,24 @@ export const Projects = () => {
       </div>
 
       {/* project 1  */}
+
+      <div className="display-projects">
+        <img className='screen' src={humano} alt="humano project img" />
+        <div className='text-container'>
+          <div className='text-project'>
+            <h5>HUMANO BARBER</h5>
+            <p>WEB DESIGN FOR A BARBER</p>
+            <p className='techs-use'>REACT - SASS</p>
+          </div>
+          <div className='btn-project'>
+            <a href="https://github.com/CrisSaavedra/The-simple-weather" target="_blank"><img src={gitHub} alt="github logo" width={20} />GITHUB</a>
+            <a href="https://master--humano-barber-demo.netlify.app/" target="_blank"><img src={live} alt="live logo" width={28} />LIVE</a>
+          </div>
+        </div>
+      </div>
+
+      {/* project 3  */}
+
       <div className="display-projects">
         <img className='screen' src={weather} alt="weather project img" />
         <div className='text-container'>
@@ -29,7 +48,7 @@ export const Projects = () => {
         </div>
       </div>
 
-      {/* project 2  */}
+      {/* project 3  */}
 
       <div className="display-projects">
         <img className='screen' src={moviet} alt="weather project img" />
