f96493417d1b95053d2c5288b11abdc19fec3afa
CrisSaavedra <crisisaave@gmail.com>
2024-11-28 19:17:45 -0400

    Refactor Portafolio.jsx: remove unused useEffect and related scroll event handling for cleaner component structure

--- src/Portafolio.jsx
+++ src/Portafolio.jsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-
 import { NavBar } from "./components/navBar/NavBar";
 import { Home } from "./components/homes/Home";
 import { Projects } from "./components/projects/Projects"
@@ -9,33 +7,8 @@ import { Contact } from "./components/contact/Contact";
 
 export const Portafolio = () => {
 
-
-    useEffect(() => {
-
-        let nav = document.getElementById('nav-container');
-        let back = document.getElementById('back');
-        let home = document.getElementById('home');
-        let menuNav = document.getElementById('nav')
-        let options = document.getElementById('options')
-
-
-        window.addEventListener("scroll", (e) => {
-            let scroll = window.scrollY;
-            if (scroll > 0) {
-                back.classList.add('background');
-            }
-            else {
-                back.classList.remove('background');
-             
-            }
-        })
-
-    }, []);
-
     return (
-
         <div >
-            <div id="back" className=""> </div>  {/* CHANGE THE BACKGROUND COLOR OF NAVBAR, IT'S FOR NOT DO ALL AGAIN  */}
             <div className="container">
                 <NavBar />
                 <Home />
