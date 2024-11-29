ce72651b28d42747f4f3b672a4718b4e6e3668eb
CrisSaavedra <crisisaave@gmail.com>
2024-11-29 13:06:44 -0400

    Add tech icons: include FastAPI, Angular, NestJS, and Python SVGs; update Home.jsx to reflect new technologies in tech showcase

--- src/components/homes/Home.jsx
+++ src/components/homes/Home.jsx
@@ -1,13 +1,18 @@
 import ts from '../../assets/techIcons/ts.svg';
 import node from '../../assets/techIcons/node.svg';
-import jsIcon from '../../assets/techIcons/javascript.svg';
 import reactIcon from '../../assets/techIcons/react.svg';
 import sass from '../../assets/techIcons/sass.svg';
 import tailIcon from '../../assets/techIcons/tailwind.svg';
 import mongo from '../../assets/techIcons/mongo.svg';
-import mysql from '../../assets/techIcons/mysql.svg';
 import postgresql from '../../assets/techIcons/postgresql.svg';
-import java from '../../assets/techIcons/java.svg';
+import angular from '../../assets/techIcons/angular.svg';
+import fastapi from '../../assets/techIcons/fastAPI.svg';
+import python from '../../assets/techIcons/python.svg';
+import nestjs from '../../assets/techIcons/nestjs.svg';
+
+// import jsIcon from '../../assets/techIcons/javascript.svg';
+// import mysql from '../../assets/techIcons/mysql.svg';
+// import java from '../../assets/techIcons/java.svg';
 
 
 export const Home = () => {
@@ -19,27 +24,26 @@ export const Home = () => {
             </div>
 
             <div className="home-text">
-                <p><span className="hello">Hola</span> 😸, Soy Cristian, el desarrollador que puede ayudar a crear lo que imaginas.</p>
+                <p><span className="hello">Hola</span> 😸, Soy Cristian, desarrollador web especializado en crear soluciones modernas utilizando Angular, React, Node.js, Python, MongoDB y PostgreSQL. </p>
             </div>
 
             <div className="tech-container">
                 <h5>TECNOLOGÍAS</h5>
                 <div className='techs techbar'>
-                    <img className='js' src={jsIcon} alt="jsIcon" width={21} />
-                    <img className='js' src={ts} alt="tsicon" width={21}/>
-                    <img src={reactIcon} alt="reactIcon" width={25}/>
+                    <img className='js' src={ts} alt="tsicon" width={21} />
+                    <img className='js' src={python} alt="htmlIcon" />
+                    <img src={reactIcon} alt="reactIcon" width={25} />
+                    <img className='js' src={angular} alt="htmlIcon" />
+                    <img src={node} alt="htmlIcon" width={25} />
+                    <img className='js' src={nestjs} alt="htmlIcon" />
+                    <img className='js' src={fastapi} alt="htmlIcon" />
+                    <img src={mongo} alt="htmlIcon" width={25} />
+                    <img className='js' src={postgresql} alt="htmlIcon" />
                     <img src={tailIcon} alt="sass" width={25} />
                     <img src={sass} alt="tailIcon" width={25} />
-                    <img src={node} alt="htmlIcon" width={25} />
-                    <img className='js' src={java} alt="htmlIcon" width={21} /> 
-                    <img src={mongo} alt="htmlIcon" width={25} /> 
-                    <img src={mysql} alt="htmlIcon" width={25} /> 
-                    <img  className='js' src={postgresql} alt="htmlIcon" width={23} /> 
-                    
 
-                                  
                 </div>
-                
+
 
             </div>
         </div>
